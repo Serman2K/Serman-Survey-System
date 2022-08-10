e4dfe312bff6140753833f64da184ca00e4df924
PSyno <psynowiecki2000@gmail.com>
2022-08-10 05:42:29 +0200

    survey name modal prototype

---
+++ js/Modal.js
@@ -0,0 +1,10 @@
+// Get the modal
+var modal = document.getElementById("modal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("modalBtn");
+
+// When the user clicks the button, open the modal 
+btn.onclick = function () {
+    modal.style.display = "block";
+}
