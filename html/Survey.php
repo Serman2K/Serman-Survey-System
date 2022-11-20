44d715f824f16df477baffd9c84a42e8f326c2e3
PSyno <psynowiecki2000@gmail.com>
2022-11-20 19:49:12 +0100

    cosmetic changes

--- html/Survey.php
+++ html/Survey.php
@@ -118,7 +118,6 @@ if($ActDate > $SurvDateStart && $ActDate < $SurvDateEnd){
     integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
     crossorigin="anonymous"></script>
 
-
 </body>
 
 </html>
