fa8fddf5cedb321118c8f3a99e1af0ffc810f350
PSyno <psynowiecki2000@gmail.com>
2022-11-21 06:58:13 +0100

    database changes

--- Database/serman_survey_system.sql
+++ Database/serman_survey_system.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 19 Lis 2022, 19:01
+-- Czas generowania: 21 Lis 2022, 06:57
 -- Wersja serwera: 10.4.24-MariaDB
 -- Wersja PHP: 8.1.6
 
@@ -50,6 +50,14 @@ CREATE TABLE `questions` (
   `date_created` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Zrzut danych tabeli `questions`
+--
+
+INSERT INTO `questions` (`id`, `survey_id`, `question`, `form_option`, `type`, `date_created`) VALUES
+(29, 30, 'sadasdsadsad', '{\"NQKUJ\":\"asasdas\",\"PLBiG\":\"dsdasdas\",\"dWgNt\":\"dadsdsd\",\"wkVSq\":\"asdasdas\",\"QjOGq\":\"sdggdf\"}', 'check_box', '2022-11-20 16:16:36'),
+(30, 30, 'sadasdsadass', '', 'text_f', '2022-11-20 16:16:39');
+
 -- --------------------------------------------------------
 
 --
@@ -67,6 +75,13 @@ CREATE TABLE `surveys` (
   `created` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Zrzut danych tabeli `surveys`
+--
+
+INSERT INTO `surveys` (`id`, `user_id`, `title`, `Folder`, `description`, `start_date`, `end_date`, `created`) VALUES
+(30, 25, 'ankieta sermana', 'FYVIkFP6L4sn', 'asdasds', '2022-11-19 16:16:00', '2022-11-21 16:16:00', '2022-11-20 16:16:19');
+
 -- --------------------------------------------------------
 
 --
@@ -79,6 +94,14 @@ CREATE TABLE `users` (
   `password` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `users`
+--
+
+INSERT INTO `users` (`id`, `user_name`, `password`, `email`) VALUES
+(25, 'Serman', '$2y$10$RAEbuR7bd4LfGU1sdbkkEuVtgf40NuqloZbMzcsK12yPKvJzc7sS6', 's.serman@gmail.com'),
+(26, 'Kuba', '$2y$10$6Lf4Sc9E.ZbbR10..nWgoesXmmVKqWK2bf.qveE1j9rAM.Eg0AejO', 'k.janicki@o2.pl');
 
 --
 -- Indeksy dla zrzutów tabel
@@ -117,25 +140,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT dla tabeli `answers`
 --
 ALTER TABLE `answers`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
 
 --
 -- AUTO_INCREMENT dla tabeli `questions`
 --
 ALTER TABLE `questions`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT dla tabeli `surveys`
 --
 ALTER TABLE `surveys`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT dla tabeli `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
